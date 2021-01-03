93516edf8ed51e300699aee8db0c448d357153e2
KimChol <kimchol2ece@outlook.com>
2021-01-03 11:09:14 +0800

    设置cookie有效期为两天+cookie有效路径

--- stu/views.py
+++ stu/views.py
@@ -5,9 +5,10 @@ from django.shortcuts import render
 
 
 def setcookie(request):
+    import datetime
     #创建响应对象
     response = HttpResponse()
     #将数据存储在cookie中。默认有效时间保存在浏览器缓存中，关闭浏览器数据丢失
     #
-    response.set_cookie('uname','zhangsan')
+    response.set_cookie('uname','zhangsan',expires=datetime.datetime.today()+datetime.timedelta(days=2),path='/student/hello')
     return response
