7ac24f630e45cb7c4e7d7b81840f4c21f2b69a56
KimChol <kimchol2ece@outlook.com>
2021-01-03 13:17:04 +0800

    加盐后获取cookie

--- stu/views.py
+++ stu/views.py
@@ -5,15 +5,12 @@ from django.shortcuts import render
 
 
 def setcookie(request):
-    import datetime
-    # 创建响应对象
     response = HttpResponse()
-    # 将数据存储在cookie中。默认有效时间保存在浏览器缓存中，关闭浏览器数据丢失
-    #
-    response.set_cookie('uname', 'zhangsan', expires=datetime.datetime.today() + datetime.timedelta(days=2),
-                        path='/student/hello')
+    response.set_signed_cookie('uname', 'zhangsan',
+                               salt='jksdfbhalefijk',
+                               max_age=24 * 60 * 60,
+                               path='/student/hello/')
     return response
-
 def getcookie(request):
-    str = request.COOKIES.get('uname')
-    return HttpResponse(str)
+    uname = request.get_signed_cookie('uname',salt='jksdfbhalefijk')
+    return HttpResponse(uname)
