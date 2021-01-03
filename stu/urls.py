998f96c86f610880a31fa1517a7c7fb4d62b0153
KimChol <kimchol2ece@outlook.com>
2021-01-03 13:05:37 +0800

    获取cookie

--- stu/urls.py
+++ stu/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, re_path, include
 from . import views
 
 urlpatterns=[
-    re_path(r'^$',views.setcookie)
+    re_path(r'^$',views.setcookie),
+    re_path(r'^hello/$',views.getcookie),
 ]
--- stu/views.py
+++ stu/views.py
@@ -5,8 +5,15 @@ from django.shortcuts import render
 
 
 def setcookie(request):
+    import datetime
+    # 创建响应对象
     response = HttpResponse()
-    response.set_signed_cookie('uname','zhangsan',
-                               salt='jksdfbhalefijk',
-                               max_age=24*60*60)
+    # 将数据存储在cookie中。默认有效时间保存在浏览器缓存中，关闭浏览器数据丢失
+    #
+    response.set_cookie('uname', 'zhangsan', expires=datetime.datetime.today() + datetime.timedelta(days=2),
+                        path='/student/hello')
     return response
+
+def getcookie(request):
+    str = request.COOKIES.get('uname')
+    return HttpResponse(str)
