eb6a75a2ff41751bc4b84b0000c7d5549c65a318
KimChol <kimchol2ece@outlook.com>
2021-01-04 22:08:29 +0800

    获取session

--- one/url.py
+++ one/url.py
@@ -3,5 +3,6 @@ from django.urls import path, include, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^$',views.setsession)
+    re_path(r'^$',views.setsession),
+    re_path(r'^get/$',views.getsession),
 ]
--- one/views.py
+++ one/views.py
@@ -8,3 +8,8 @@ def setsession(request):
     #设置有效时间。参数类型：   整数：秒 设置0代表关闭浏览器之后过期  日期：指定日期值过期  None:默认时间过期
     request.session.set_expiry(3*24*60*60)
     return HttpResponse('设置成功！')
+
+
+def getsession(request):
+    uname = request.session['uname']
+    return HttpResponse(uname)
