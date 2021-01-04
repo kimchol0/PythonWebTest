866c58f585eb0d090b53bab64aaddfa0bc2f89e1
KimChol <kimchol2ece@outlook.com>
2021-01-04 22:10:14 +0800

    删除指定的session

--- one/views.py
+++ one/views.py
@@ -7,6 +7,7 @@ def setsession(request):
     request.session['uname']='zhangsan'
     #设置有效时间。参数类型：   整数：秒 设置0代表关闭浏览器之后过期  日期：指定日期值过期  None:默认时间过期
     request.session.set_expiry(3*24*60*60)
+    del request.session['uname']
     return HttpResponse('设置成功！')
 
 
