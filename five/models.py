96cfa419d5c6843d83dd5943fca47bd28fddf25f
KimChol <kimchol2ece@outlook.com>
2021-01-10 19:40:41 +0800

    网页菜单栏实现——创建model文件

--- five/models.py
+++ five/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Menu(models.Model):
+    mid = models.AutoField(primary_key=True)
+    mname = models.CharField(max_length=30,unique=True)
+
+    def __unicode__(self):
+        return u'Menu:%s'%self.mname
+    class Meta:
+        db_table = 't_menu'
