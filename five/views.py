f286963a668d555e8790ea8ad349d4a0d900930d
KimChol <kimchol2ece@outlook.com>
2021-01-10 19:31:12 +0800

    全局上下文的实现(模板页面方式)

--- five/views.py
+++ five/views.py
@@ -1,9 +1,19 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.template import Template, RequestContext
 from django.views import View
+from five.my_context_processors import getData
 
 
 class IndexView(View):
     def get(self,request):
         return render(request,'index3.html')
+
+
+class Index2View(View):
+    def get(self,request):
+        t = Template('{{uname}}')
+        str = t.render(RequestContext(request,dict_= None,processors=(getData,)))
+        return HttpResponse(str)
