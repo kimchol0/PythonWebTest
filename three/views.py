fed969f73de7e423a5990f404624a0e4e9a0619a
KimChol <kimchol2ece@outlook.com>
2021-01-10 13:14:11 +0800

    模板标签语法（块标签）——自动转义为off其他情况

--- three/views.py
+++ three/views.py
@@ -7,9 +7,10 @@ from django.views import View
 class GetDataView(View):
     def get(self,request):
         import datetime
+        loc = '<script>location.href="/admin"</script>'
         return render(request,'getdata1.html',{'user':{'uname':'zhangsan','pwd':'123'},
                                                'numlist':[1,2,3,4,5],
                                                'current':datetime.datetime.today(),
                                                'str':'hello','score': 88 ,
-                                               'loc': '<h3>北京</h3>',
+                                               'loc': loc,
                                                })
