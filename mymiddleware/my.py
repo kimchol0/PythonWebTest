0ab81dba8326b4208ef9ca347aeebe83571f45ca
KimChol <kimchol2ece@outlook.com>
2021-01-11 16:05:49 +0800

    自定义中间件_

--- mymiddleware/my.py
+++ mymiddleware/my.py
@@ -4,6 +4,12 @@ from django.utils.deprecation import MiddlewareMixin
 class Row1(MiddlewareMixin):
     def process_request(self,request):
         print("中间件1")
+
+    def process_view(self,request,view_func,view_func_args,
+                     view_func_kwargs):
+        #view_func对应views函数，view_func_args,kwargs对应views里的参数
+        print("中间件1view")
+
     def process_response(self,request,response):
         print("中间件1返回")
         return response
@@ -13,6 +19,12 @@ class Row1(MiddlewareMixin):
 class Row2(MiddlewareMixin):
     def process_request(self,request):
         print("中间件2")
+
+    def process_view(self,request,view_func,view_func_args,
+                     view_func_kwargs):
+        #view_func对应views函数，view_func_args,kwargs对应views里的参数
+        print("中间件2view")
+
     def process_response(self,request,response):
         print("中间件2返回")
         return response
@@ -20,6 +32,12 @@ class Row2(MiddlewareMixin):
 class Row3(MiddlewareMixin):
     def process_request(self,request):
         print("中间件3")
+
+    def process_view(self,request,view_func,view_func_args,
+                     view_func_kwargs):
+        #view_func对应views函数，view_func_args,kwargs对应views里的参数
+        print("中间件3view")
+
     def process_response(self,request,response):
         print("中间件3返回")
         return response
